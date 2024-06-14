2e20e5b693d84b5f531b6a91cf6676172215f2c8
noooooooooooooooob <168753086+noooooooooooooooob@users.noreply.github.com>
2024-06-14 14:48:13 +0900

    Add files via upload

---
+++ 코드들/22 . KeyboardAndTimer/ChildView.cpp
@@ -0,0 +1,152 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "KeyboardAndTimer2.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+	m_pt = CPoint(100, 100);
+	m_dx = 0;
+	m_dy = 0;
+	m_size = 50;
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_KEYDOWN()
+	ON_WM_CHAR()
+	ON_WM_CREATE()
+	ON_WM_TIMER()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+	dc.Rectangle(m_pt.x - m_size, m_pt.y - m_size, 
+		         m_pt.x + m_size, m_pt.y + m_size);
+
+
+
+	dc.TextOutW(0, 0, m_str);
+}
+
+
+
+void CChildView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	// Virtual Key Code 'A'~'Z' '0'~'9' 
+
+	if (nChar == VK_SPACE) {
+		CString str;
+		str.Format(_T("Key = %d"), nChar);
+		MessageBox(str);
+	}
+	if (nChar == VK_BACK) {
+		m_str.Delete(m_str.GetLength() - 1, 1);
+		Invalidate();
+	}
+	switch (nChar) {
+	case VK_LEFT:	m_dx = -10;	break;
+	case VK_RIGHT:	m_dx = 10; 	break;
+	case VK_UP:		m_dy = -10;	break;
+	case VK_DOWN:	m_dy = 10;	break;
+	default:									break;
+	}
+	Invalidate();
+
+
+
+	CWnd::OnKeyDown(nChar, nRepCnt, nFlags);
+}
+
+
+void CChildView::OnChar(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	if (nChar != VK_BACK) {
+		m_str += TCHAR(nChar);
+		Invalidate();
+	}
+
+	CWnd::OnChar(nChar, nRepCnt, nFlags);
+}
+
+
+int CChildView::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CWnd::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	SetTimer(0, 30, NULL);
+	SetTimer(1, 10, NULL);
+	
+	return 0;
+}
+
+void CChildView::OnTimer(UINT_PTR nIDEvent)
+{
+	if (nIDEvent == 0) {
+		m_pt.x += m_dx;
+		m_pt.y += m_dy;
+
+		if (m_pt.x - m_size < 0) {	m_pt.x = m_size; m_dx = -m_dx;	}
+		if (m_pt.y - m_size < 0) { m_pt.y = m_size; m_dy = -m_dy; }
+
+		CRect rect;
+		GetClientRect(rect);
+		int w = rect.Width();
+		int h = rect.Height();
+
+		if (m_pt.x + m_size> w) { 
+			m_pt.x = w - m_size; 
+			m_dx = -m_dx; 
+		}
+		if (m_pt.y + m_size > h) {
+			m_pt.y = h - m_size;
+			m_dy = -m_dy;
+		}
+		Invalidate();
+	}
+	else if (nIDEvent == 1) {
+		static float time = 0.0f;
+		time += 0.1;
+		m_size = 50 + 30 * sin(time);
+	}
+
+
+
+	CWnd::OnTimer(nIDEvent);
+}
---
+++ 코드들/22 . KeyboardAndTimer/ChildView.h
@@ -0,0 +1,47 @@
+﻿
+// ChildView.h: CChildView 클래스의 인터페이스
+//
+
+
+#pragma once
+
+
+// CChildView 창
+
+class CChildView : public CWnd
+{
+// 생성입니다.
+public:
+	CChildView();
+
+// 특성입니다.
+public:
+	CString m_str;
+
+	CPoint m_pt;
+	int m_dx;
+	int m_dy;
+	int m_size;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+	protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// 구현입니다.
+public:
+	virtual ~CChildView();
+
+	// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnPaint();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg void OnChar(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+};
+
