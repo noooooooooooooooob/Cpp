76a83a655fc4535a9777402187a4c65f5fa01602
noooooooooooooooob <168753086+noooooooooooooooob@users.noreply.github.com>
2024-06-14 14:44:39 +0900

    Add files via upload

---
+++ 코드들/19 . BitmapMFC/ChildView.cpp
@@ -0,0 +1,102 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "BitmapMFC1.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_ERASEBKGND()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+	CRect rect;
+	GetClientRect(rect);
+
+	CDC memDC;
+	memDC.CreateCompatibleDC(&dc);
+	CBitmap memBMP;
+	memBMP.CreateCompatibleBitmap(&dc, rect.Width(), rect.Height());
+	memDC.SelectObject(memBMP);
+
+	memDC.Rectangle(rect);
+
+	CBitmap bmp;
+	bmp.LoadBitmapW(IDB_BITMAP2);
+
+	BITMAP bmpInfo;
+	bmp.GetBitmap(&bmpInfo);
+
+	int w = bmpInfo.bmWidth;
+	int h = bmpInfo.bmHeight;
+
+	// RAM --> VRAM (BLOCK Transfer - blt)
+	CDC bmpDC;
+	bmpDC.CreateCompatibleDC(&dc);
+	bmpDC.SelectObject(bmp);
+
+	//dc.BitBlt(100, 100, w, h, &memDC, 0, 0, SRCCOPY);
+
+	memDC.StretchBlt(0,0,rect.Width(), rect.Height(), 
+		          &bmpDC, 0, 0, w, h, SRCCOPY);
+
+	for (int x = 0; x < 256; x++)
+		for (int y = 0; y < 256; y++)
+			memDC.SetPixel(x, y, RGB(x, 0, y));
+
+	dc.BitBlt(0, 0, rect.Width(), rect.Height(), &memDC, 0, 0, SRCCOPY);
+
+/*
+	CBrush brush(&bmp);
+	dc.SelectObject(brush);
+
+	CRect rect;
+	GetClientRect(rect);
+	dc.Rectangle(0, 0, bmpInfo.bmWidth+100, bmpInfo.bmHeight+100);
+*/
+}
+
+BOOL CChildView::OnEraseBkgnd(CDC* pDC)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	return true;
+	//return CWnd::OnEraseBkgnd(pDC);
+}
---
+++ 코드들/19 . BitmapMFC/resource.h
@@ -0,0 +1,26 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// BitmapMFC1.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_BitmapMFC1TYPE              130
+#define IDB_BITMAP1                     311
+#define IDB_BITMAP2                     312
+#define ID_MYMENU_MENU1                 32771
+#define ID_MYMENU_MENU2                 32772
+#define ID_MYMENU_MENU3                 32773
+#define ID_MENU3_SUBMENU1               32774
+#define ID_MENU3_SUBMENU2               32775
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        313
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
