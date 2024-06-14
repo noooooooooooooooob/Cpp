7a23c66da1c509018ed214ccb3ea1af5112cdf1e
noooooooooooooooob <168753086+noooooooooooooooob@users.noreply.github.com>
2024-06-14 14:39:27 +0900

    Create Hello.h

---
+++ 코드들/17 . HelloMFC2/Hello.h
@@ -0,0 +1,16 @@
+class CMyApp : public CWinApp
+{
+public:
+    virtual BOOL InitInstance ();
+};
+
+class CMainWindow : public CFrameWnd
+{
+public:
+    CMainWindow ();
+
+protected:
+    afx_msg void OnPaint ();
+    DECLARE_MESSAGE_MAP ()
+};
+
